f2edd7cd6aeeae96cd6f3f4659be9ff3ff50c3c5
andylamax <andylamax@programmer.net>
2020-12-04 04:47:26 +0300

    Modified workfile

--- applikation-runtime/build.gradle.kts
+++ applikation-runtime/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.library") version "4.1.0"
     kotlin("multiplatform") version "1.4.20"
     id("io.codearte.nexus-staging") version "0.22.0"
-    id("tz.co.asoft.library") version "1.0.0"
+    id("tz.co.asoft.library")
     signing
 }
 
