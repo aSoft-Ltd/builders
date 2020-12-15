f25d28b209deb18606f4d1ef8714597327f94b6e
andylamax <andylamax@programmer.net>
2020-12-15 15:35:17 +0300

    Bumped version

--- gradle-plugins/src/main/kotlin/versions.kt
+++ gradle-plugins/src/main/kotlin/versions.kt
@@ -1,5 +1,7 @@
 object versions {
-    val kotlin = "1.4.20"
-    val builders = "1.0.0"
-    val android_build_tools = "4.1.0"
+    val kotlin = "1.4.21"
+    val agp = "4.1.0"
+    val nexus_staging = "0.22.0"
+    val asoft_builders = "1.2.0"
+    val asoft_mapper = "0.0.2"
 }
