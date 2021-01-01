25281ba5d2a1d47c8496359e60e4ade25bb95d07
andylamax <andylamax@programmer.net>
2021-01-01 03:24:12 +0300

    Ignored timeout test as they take long to finish

--- samples/applikation-samples/multiplatform-applikation/src/jsTest/kotlin/TimeoutTest.kt
+++ samples/applikation-samples/multiplatform-applikation/src/jsTest/kotlin/TimeoutTest.kt
@@ -1,9 +1,11 @@
 import kotlinx.coroutines.delay
 import tz.co.asoft.asyncTest
+import kotlin.test.Ignore
 import kotlin.test.Test
 
 class TimeoutTest {
     @Test
+    @Ignoregit
     fun should_stay_for_at_least_ten_seconds() = asyncTest {
         console.log("Testing for a long time")
         delay(9500)
@@ -11,6 +13,7 @@ class TimeoutTest {
     }
 
     @Test
+    @Ignore
     fun assert_it_due_to_timout() = asyncTest {
         console.log("Testing for a long time")
         delay(10500)
