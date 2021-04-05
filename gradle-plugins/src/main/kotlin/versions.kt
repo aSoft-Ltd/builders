49ce94f57a0c9907b6fa3bbadf529e16c1278102
andylamax <andylamax@programmer.net>
2021-04-05 03:13:20 +0300

    Building with kotlin 1.3.31

--- gradle-plugins/src/main/kotlin/versions.kt
+++ gradle-plugins/src/main/kotlin/versions.kt
@@ -1,10 +1,10 @@
 object versions {
-    val kotlin = "1.4.21"
+    val kotlin = "1.4.31"
     val agp = "4.1.3"
     val nexus_staging = "0.22.0"
 
     object asoft {
-        val builders = "1.3.20"
+        val builders = "1.3.21"
         val mapper = "0.0.60"
     }
 }
